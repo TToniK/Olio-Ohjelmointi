fc60daf7eb2acb956d3683d7665199148899adbf
Toni Kari <kari.toni@hotmail.com>
2019-01-09 09:41:19 +0200

    kirjaluokka-tehtävä tehty

---
+++ Kirjaluokka/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kirjaluokka
+{
+    class Book
+    {
+
+        public string title;
+        public string author;
+        public string id;
+        public double price;
+
+       
+        public Book(string title, string author, string id, int price)
+        {
+            this.title = title;
+            this.author = author;
+            this.id = id;
+            this.price = price;
+            
+        }
+        public void Printinfo()
+        {
+            Console.WriteLine($"Kirjan tiedot:\nNimi: {this.title}" + 
+                $"\nKirjailija: {this.author}\n" + 
+                $"Id: {this.id}\n" + 
+                $"Hinta: {this.price.ToString()} euroa\n");
+        }
+        public void CompareBook(Book book)
+        {
+            if (this.price > book.price)
+                Console.WriteLine($"Kirja {this.title} on kalliimpi kuin {book.title}");
+            else
+                Console.WriteLine($"Kirja {book.title} on kalliimpi kuin {this.title}");
+        }
+    }
+}
+  
---
+++ Kirjaluokka/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Kirjaluokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma antaa kirjojen tiedot ja vertailee hinnat\n");
+            Book myBook = new Book("ABC", "Toni K", "00155", 10);
+            myBook.Printinfo();
+
+            Book myBook2 = new Book("CBA", "Kari T", "00255", 25);
+            myBook2.Printinfo();
+            myBook.CompareBook(myBook2);
+            Console.WriteLine("Paina mitä vain lopettaaksesi....");
+            Console.ReadKey();
+
+        }
+    }
+}
+
