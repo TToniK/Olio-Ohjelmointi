2b4da410054b1dd7215b469a0c511cb5f5d4c45f
Toni Kari <kari.toni@hotmail.com>
2019-01-14 10:15:18 +0200

    Firmaluokka DONE

---
+++ Firmaluokka/Firmaluokka/Company.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Firmaluokka
+{
+    class Company
+    {
+        string message = null;
+
+        public Company()
+        {
+            this.Title = "Ei nimeä";
+            this.Address = "Ei osoitetta";
+            this.Phone = "Ei numeroa";
+            this.Income = 0;
+            this.Expense = 0;
+        }
+
+       public Company(Company previousCompany)
+       {
+            Title = previousCompany.Title;
+            Address = previousCompany.Address;
+            Phone = previousCompany.Phone;
+            Income = previousCompany.Income;
+            Expense = previousCompany.Expense;
+       }
+
+
+        public Company(string title, string address, string phone, int income, int expense)
+        {
+            Title = title;
+            Address = address;
+            Phone = phone;
+            Income = income;
+            Expense = expense;
+        }
+            public string Title { get; set; }
+            public string Address { get; set; }
+            public string Phone { get; set; }
+            public int Income { get; set; }
+            public int Expense { get; set; }
+
+        public string Info()
+        {
+            return "Firma: " + Title + "\nOsoite: " + Address + "\nPuhelinnumero: " + Phone.ToString() + "\nTulot: "
+                + Income.ToString() + "\nMenot: " + Expense.ToString();
+        }
+
+        public string Win()
+        {
+            double winloss = (this.Income - this.Expense) / this.Expense * 100;
+            if (winloss <= 100)
+            {
+                message = "Tulosprosentti: " + winloss + "%,\nyrityksellä menee kehnosti";
+            }
+            else if (winloss < 100 && winloss <= 200)
+            {
+                message = "Tulosprosentti: " + winloss + "%,\nyrityksellä menee tyydyttävästi";
+            }
+            if(winloss >200)
+            {
+                message = "Tulosprosentti: " + winloss + "%,\nyrityksellä menee hyvin";
+            }
+            return message;
+         
+           
+        }
+    }       
+}
---
+++ Firmaluokka/Firmaluokka/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Firmaluokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä on Firma-ohjelma\n\n");
+            Company company1 = new Company("Leipomo", "Lappeenkatu 11","0404455",100000,50000);
+            Company company2 = new Company(company1)
+            {
+                Title = "Kahvila",
+                Address = "Valtakatu 5",
+                Phone = "050654128",
+                Income = 500000,
+                Expense = 50000
+            };
+            Console.WriteLine(company1.Info());
+            Console.WriteLine(company1.Win());
+            Console.WriteLine("\n------------------------------\n");
+            Console.WriteLine(company2.Info());
+            Console.WriteLine(company2.Win());
+            Console.ReadKey();
+        }
+    }
+}
