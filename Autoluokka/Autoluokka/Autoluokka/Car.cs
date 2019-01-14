eed1003ef85f7e95db7afcc9f92becd86da0719d
Toni Kari <kari.toni@hotmail.com>
2019-01-14 10:32:49 +0200

    Autoluokka DONE

---
+++ Autoluokka/Autoluokka/Autoluokka/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Autoluokka
+{
+    class Car
+    {
+        public string brand;
+        public int speed;
+
+        public Car()
+        {
+            this.brand = "";
+            this.speed = 0;
+        }
+
+        public void AskData()
+        {
+            
+            Console.WriteLine("Merkki: ");
+            this.brand = Console.ReadLine();
+            Console.WriteLine("Huippunopeus (km/h): ");
+            this.speed = int.Parse(Console.ReadLine());
+        }    
+        
+        public void Printinfo()
+        {
+            Console.WriteLine($"Auton tiedot:\nMerkki: {this.brand}" +
+                $"\nHuippunopeus: {this.speed}km/h\n");
+
+        }
+
+        public void Accelerate()
+        {
+            Console.WriteLine("Laita 0, jos et halua kiihdyttää, muulloin kiihdytä: ");
+            this.speed += int.Parse(Console.ReadLine());
+        }
+        public void Brake()
+        {
+            Console.WriteLine("Haluatko jarruttaa? 1 = kyllä tai 2 = ei");
+            if (int.Parse(Console.ReadLine()) == 1)
+            {
+                double braking = (this.speed * 90) / 100;
+                Console.WriteLine($"Jarrutus suoritettu, uusi nopeus: {braking}km/h");
+            }
+            else if (int.Parse(Console.ReadLine()) == 2)
+            {
+                Console.WriteLine("Jarrutusta ei suoritettu");
+            }
+            else
+            { 
+                Console.WriteLine("Väärä syöte!");
+            }
+        }
+    }
+}
---
+++ Autoluokka/Autoluokka/Autoluokka/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Autoluokka
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.AskData();
+            Car myCar2 = new Car();
+            Console.WriteLine("\n ");
+            Console.WriteLine("Toisen auton tiedot\n ");
+            myCar2.AskData();
+            Console.WriteLine("\n ");
+            myCar.Accelerate();
+            myCar.Brake();
+            myCar.Printinfo();
+            myCar2.Printinfo();
+            
+
+            Console.ReadKey();
+        }
+    }
+}
