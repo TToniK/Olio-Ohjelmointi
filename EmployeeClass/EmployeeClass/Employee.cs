89e21a5a0cb8787db598db3f821b1bea2c4d97e9
Toni Kari <TToniK@users.noreply.github.com>
2019-01-17 11:07:37 +0200

    EmployeeClass Done

---
+++ EmployeeClass/EmployeeClass/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeClass
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Position { get; set; }
+        public int Salary { get; set; }
+        private static int counter = 1;
+        private string message = null;
+
+        public Employee()
+        {
+            this.Name = System.String.Empty;
+            this.Id = 0;
+            this.Position = System.String.Empty;
+            this.Salary = 0;
+        }
+
+        public Employee(string name, int id, string position, int salary)
+        {
+            this.Name = name;
+            this.Id = counter++;
+            this.Position = position;
+            this.Salary = salary;
+        }
+
+        public string CompareSalary(Employee employee)
+        {
+            if (this.Salary > employee.Salary)
+            {
+                message = $"{this.Name} tienaa enemmän kuin {employee.Name}";
+            }
+            else if (this.Salary == employee.Salary)
+            {
+                message = $"{this.Name} tienaa yhtä paljon kuin {employee.Name}";
+            }
+            else
+            {
+                message = $"{employee.Name} tienaa enemmän kuin {this.Name}";
+            }
+            return message;
+                
+            
+        }
+        public string PrintEmployeeInfo()
+        {
+            return $"Työntekijä: {this.Name}, Id: {this.Id}, Tehtävä: {this.Position}, Palkka: {this.Salary}";
+        }
+
+    }
+}
---
+++ EmployeeClass/EmployeeClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä on työntekijä ohjelma\n\n");
+            Employee[] employees = new Employee[3];
+            employees[0] = new Employee("Pekka", 1, "myyntipäällikkö", 3500);
+            employees[1] = new Employee("Matti", 1, "pomo", 5500);
+            employees[2] = new Employee("Marja", 1, "sihteeri", 2500);
+            Console.WriteLine(employees[0].PrintEmployeeInfo());
+            Console.WriteLine(employees[1].PrintEmployeeInfo());
+            Console.WriteLine(employees[2].PrintEmployeeInfo());
+            Console.WriteLine(employees[0].CompareSalary(employees[1]));
+            Console.WriteLine(employees[1].CompareSalary(employees[2]));
+            Console.WriteLine(employees[2].CompareSalary(employees[0]));
+            Console.ReadKey();
+        }
+    }
+}
